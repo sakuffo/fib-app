8c6024af788495552c8f47ac2e32b680a44a94a0
Stephen Akuffo <sa.akuffo@gmail.com>
2022-01-02 13:18:44 -0500

    first app try

--- client/src/App.js
+++ client/src/App.js
@@ -19,13 +19,12 @@ function App() {
           >
             Learn React
           </a>
-
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
         </header>
         <div>
-            <Route exact path="/" component={Fib} />
-            <Route path="/otherpage" component={OtherPage} />
+          <Route exact path="/" component={Fib} />
+          <Route path="/otherpage" component={OtherPage} />
         </div>
       </div>
     </Router>
