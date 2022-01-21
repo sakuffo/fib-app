8c7da7d102cf623086e20e873cc92e30e4881146
Stephen Akuffo <sa.akuffo@gmail.com>
2022-01-21 02:31:08 -0500

    testing fix

--- worker/index.js
+++ worker/index.js
@@ -1,13 +1,11 @@
 const keys = require('./keys');
 const redis = require('redis');
 
-
 const redisClient = redis.createClient({
     host: keys.redisHost,
     port: keys.redisPort,
     retry_strategy: () => 1000
 });
-
 const sub = redisClient.duplicate();
 
 function fib(index) {
